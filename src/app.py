80c039fc503a9468c156ea74727880f105555ea4
JennGuz <143773875+JennGuz@users.noreply.github.com>
2024-10-26 01:39:04 +0000

    project completed

--- src/app.py
+++ src/app.py
@@ -1,7 +1,42 @@
 import os
 import pandas as pd
-import seaborn as sns
+import matplotlib.pyplot as plt
 from dotenv import load_dotenv
-
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyClientCredentials
 # load the .env file variables
 load_dotenv()
+
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
+
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+conn = Spotify(client_credentials_manager=client_credentials_manager)
+
+artist_id = '4GvEc3ANtPPjt1ZJllr5Zl'
+
+results = conn.artist_top_tracks(artist_id)
+
+if results:
+    tracks = results['tracks'][:10]
+    top_tracks_info = [
+        {
+            'name': track['name'],
+            'popularity': track['popularity'],
+            'duration_minutes': (track['duration_ms'] / (1000 * 60)) 
+        }
+        for track in tracks
+    ]
+
+    df_tracks = pd.DataFrame(top_tracks_info)
+    
+    print(df_tracks)
+
+plt.figure(figsize=(10,6))
+plt.scatter(df_tracks['duration_minutes'], df_tracks['popularity'], color='blue')
+
+plt.title('scatter plot of popularity vs duration')
+plt.xlabel('duration(min)')
+plt.ylabel('popularity')
+plt.grid(True)
+plt.show()
